daeb190f0156457f77da7936a5060640db5d401c
azarrias <adolfo.zarrias@gmail.com>
2020-11-08 21:06:00 +0100

    Refactor circle shader and create smiley fragment shader

--- src/shaders/smiley.fs
+++ src/shaders/smiley.fs
@@ -10,13 +10,20 @@ float Circle(vec2 uv, vec2 pos, float radius, float step_size)
 
 vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords)
 {
-  vec2 uv = vec2(screen_coords.x / resolution.x, screen_coords.y / resolution.y); // normalize coords
+  vec2 uv = vec2(screen_coords.x / resolution.x, screen_coords.y / resolution.y);
   uv -= .5;                             // make uv range -0.5 to 0.5
   uv.x *= resolution.x / resolution.y;  // multiply x by aspect ratio to compensate for the width vs height difference
   
-  float c = Circle(uv, vec2(0.), .4, 0.05);  // circle shape
-  vec3 col = vec3(1., 0., 1.);
-  col *= c;
+  vec3 col = vec3(1., 1., 0.);                    // yellow color
+  float mask = Circle(uv, vec2(0.), .4, 0.05);    // circle shape
+  mask -= Circle(uv, vec2(-.13, -.2), .07, .01);  // left eye
+  mask -= Circle(uv, vec2(.13, -.2), .07, .01);   // right eye
+  
+  float mouth = Circle(uv, vec2(0.), .3, .02);
+  mouth -= Circle(uv, vec2(0., -0.1), .3, .02);
+  
+  mask -= mouth;  
+  col *= mask;
   
   return vec4(col, 1);              // use the same c value for r, g and b
 }
