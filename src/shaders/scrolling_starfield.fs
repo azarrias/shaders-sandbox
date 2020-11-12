795a5379f66bbdc10b993dd0203d6ce322ff477b
azarrias <adolfo.zarrias@gmail.com>
2020-11-12 11:55:50 +0100

    Create grid UVs and randomize position of stars within each cell of the grid

--- src/shaders/scrolling_starfield.fs
+++ src/shaders/scrolling_starfield.fs
@@ -7,6 +7,14 @@ mat2 Rotate(float a) {
   return mat2(c, -s, s, c);
 }
 
+// pseudo-random number generator, 
+// takes 2 inputs, returns 1 value (0 to 1)
+float Hash(vec2 p) {
+  p = fract(p * vec2(123.34, 456.21));
+  p += dot(p, p + 45.32);
+  return fract(p.x * p.y);                    
+}
+
 float Star(vec2 uv, float flare) {
   float dc = length(uv);                      // distance to center
   float star = .05 / dc;                      // asymptote for star glow
@@ -19,7 +27,7 @@ float Star(vec2 uv, float flare) {
   sparkle = 1. - abs(uv.x * uv.y * 1000);     // rotated star sparkle
   sparkle = max(sparkle, 0);                  // avoid it to subtract color from the star
   star += sparkle * flare * .3;               // make it less bright
-  star *= smoothstep(.5, .2, dc);             // fade out star, remove long rotated sparkle
+  star *= smoothstep(1., .2, dc);             // fade out star, remove long rotated sparkle
   
   return star;
 }
@@ -28,9 +36,24 @@ vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) 
   vec2 uv = screen_coords / resolution - .5;  // normalize uv
   uv.x *= resolution.x / resolution.y;        // compensate for aspect ratio
   uv *= 3.;                                   // zoom out
+  
   vec3 col = vec3(0.05, 0.05, 0.05);          // background color
+
+  vec2 gv = fract(uv);                        // grid uv with fractional component of uv
+  gv -= .5;                                   // remap origin to center for each cell
+  vec2 id = floor(uv);                        // cell id, integer components of uv
   
-  col += Star(uv, 1.) + time * 0.000000000001;
-  
+  for (int y = -1; y <= 1; y++) {             // iterate through cell neighbors to get
+    for (int x = -1; x <= 1; x++) {           // light contribution from one cell to the other
+      vec2 cell_offset = vec2(x, y);          // cell offset to the neighbor
+      float rand_x = Hash(id + cell_offset);  // random num from 0 to 1
+      float rand_y = fract(rand_x * 34.);
+      rand_x -= .5;                           // range random num from -.5 to .5
+      rand_y -= .5;      
+      
+      col += Star(gv - cell_offset - vec2(rand_x, rand_y), 1.) + time * 0.000000000001;
+    }
+  }
+ 
   return vec4(col, 1);              // use the same c value for r, g, b
 }
